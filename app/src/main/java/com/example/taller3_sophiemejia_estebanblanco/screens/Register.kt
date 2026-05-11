fea4fbc615081e2f4d2079d4512a00df3de03518
sophieMjs <sophie.mejia.sc@gmail.com>
2026-05-11 15:37:48 -0500

    reg

--- app/src/main/java/com/example/taller3_sophiemejia_estebanblanco/screens/Register.kt
+++ app/src/main/java/com/example/taller3_sophiemejia_estebanblanco/screens/Register.kt
@@ -1,56 +1,71 @@
 package com.example.taller3_sophiemejia_estebanblanco.screens
 
 import android.util.Log
-
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.navigation.NavController
-import com.example.taller3_sophiemejia_estebanblanco.shared.MyButton
-import com.example.taller3_sophiemejia_estebanblanco.model.User
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.taller3_sophiemejia_estebanblanco.model.User
 import com.example.taller3_sophiemejia_estebanblanco.navigation.AppScreens
+import com.example.taller3_sophiemejia_estebanblanco.shared.MyButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
+data class RegisterState(
+    val name: String = "",
+    val lastname: String = "",
+    val id: String = "",
+    val email: String = "",
+    val password: String = "",
+    val profilepic: String = ""
+)
 
+class RegisterViewModel : ViewModel() {
+    private val _state = MutableStateFlow(RegisterState())
+    val state = _state.asStateFlow()
 
+    fun updateName(newValue: String) {
+        _state.update { it.copy(name = newValue) }
+    }
+
+    fun updateLastname(newValue: String) {
+        _state.update { it.copy(lastname = newValue) }
+    }
+
+    fun updateId(newValue: String) {
+        _state.update { it.copy(id = newValue) }
+    }
+
+    fun updateEmail(newValue: String) {
+        _state.update { it.copy(email = newValue) }
+    }
+
+    fun updatePassword(newValue: String) {
+        _state.update { it.copy(password = newValue) }
+    }
+}
 
 @Composable
-fun Register(controller: NavController) {
-    var name by remember { mutableStateOf("") }
-    var lastname by remember { mutableStateOf("") }
-    var id by remember { mutableStateOf("") }
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var latitude by remember { mutableStateOf("") }
-    var longitude by remember { mutableStateOf("") }
-
+fun Register(controller: NavController, viewModel: RegisterViewModel = viewModel()) {
+    val state by viewModel.state.collectAsState()
     val auth = FirebaseAuth.getInstance()
+    val database = FirebaseDatabase.getInstance()
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item {
             Column(
@@ -59,79 +74,75 @@ fun Register(controller: NavController) {
                     .padding(16.dp),
                 horizontalAlignment = Alignment.Start
             ) {
-                Text(text = "Nombres", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Text("Nombres", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                 OutlinedTextField(
-                    value = name,
-                    onValueChange = { name = it },
+                    value = state.name,
+                    onValueChange = { viewModel.updateName(it) },
                     modifier = Modifier.fillMaxWidth(),
-                    label = { Text("Ingrese su nombre") }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
+                    label = { Text("Ingrese su nombre") })
+                Spacer(Modifier.height(16.dp))
 
-                Text(text = "Apellidos", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Text("Apellidos", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                 OutlinedTextField(
-                    value = lastname,
-                    onValueChange = { lastname = it },
+                    value = state.lastname,
+                    onValueChange = { viewModel.updateLastname(it) },
                     modifier = Modifier.fillMaxWidth(),
-                    label = { Text("Ingrese su apellido") }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
+                    label = { Text("Ingrese su apellido") })
+                Spacer(Modifier.height(16.dp))
 
-                Text(text = "No. de Identificación", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Text("No. de Identificación", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                 OutlinedTextField(
-                    value = id,
-                    onValueChange = { id = it },
+                    value = state.id,
+                    onValueChange = { viewModel.updateId(it) },
                     modifier = Modifier.fillMaxWidth(),
-                    label = { Text("Ingrese su número de identificación") }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
+                    label = { Text("Ingrese su identificación") })
+                Spacer(Modifier.height(16.dp))
 
-                Text(text = "Email", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Text("Email", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                 OutlinedTextField(
-                    value = email,
-                    onValueChange = { email = it },
+                    value = state.email,
+                    onValueChange = { viewModel.updateEmail(it) },
                     modifier = Modifier.fillMaxWidth(),
-                    label = { Text("Ingrese su email") }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
+                    label = { Text("Ingrese su email") })
+                Spacer(Modifier.height(16.dp))
 
-                Text(text = "Contraseña", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Text("Contraseña", fontSize = 20.sp, fontWeight = FontWeight.Bold)
                 OutlinedTextField(
-                    value = password,
-                    onValueChange = { password = it },
+                    value = state.password,
+                    onValueChange = { viewModel.updatePassword(it) },
                     modifier = Modifier.fillMaxWidth(),
-                    label = { Text("Ingrese su contraseña") }
-                )
-                Spacer(modifier = Modifier.height(32.dp))
+                    label = { Text("Ingrese su contraseña") })
+                Spacer(Modifier.height(32.dp))
 
                 MyButton(text = "Registrarse") {
-                    val database = FirebaseDatabase.getInstance()
-
-                    auth.createUserWithEmailAndPassword(email, password)
+                    auth.createUserWithEmailAndPassword(state.email, state.password)
                         .addOnCompleteListener { task ->
                             if (task.isSuccessful) {
-                                val userAuth = auth.currentUser
-
-                                userAuth?.let { firebaseUser ->
-                                    val upcrb = UserProfileChangeRequest.Builder()
-                                    upcrb.setDisplayName("$name $lastname")
-                                    firebaseUser.updateProfile(upcrb.build())
+                                val firebaseUser = auth.currentUser
+                                firebaseUser?.let { user ->
+                                    val profileUpdates = UserProfileChangeRequest.Builder()
+                                        .setDisplayName("${state.name} ${state.lastname}").build()
+                                    user.updateProfile(profileUpdates)
 
                                     val nuevoUsuario = User(
-                                        name = name,
-                                        lastname = lastname,
-                                        id = id,
-                                        email = email,
-                                        latitude = latitude.toDoubleOrNull() ?: 0.0,
-                                        longitude = longitude.toDoubleOrNull() ?: 0.0
+                                        name = state.name,
+                                        lastname = state.lastname,
+                                        id = state.id,
+                                        email = state.email,
+                                        password = state.password,
+                                        latitude = 0.0,
+                                        longitude = 0.0,
+                                        available = true,
+                                        profilepic = state.profilepic
                                     )
 
-                                    val myRef = database.getReference("users/" + firebaseUser.uid)
-                                    myRef.setValue(nuevoUsuario)
-                                    controller.navigate(AppScreens.home.name)
+                                    database.getReference("users/${user.uid}")
+                                        .setValue(nuevoUsuario).addOnCompleteListener {
+                                            controller.navigate(AppScreens.home.name)
+                                        }
                                 }
                             } else {
-                                Log.e("MYTAG", "Error: " + task.exception?.message)
+                                Log.e("Register", "Error: ${task.exception?.message}")
                             }
                         }
                 }
